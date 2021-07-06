cb25c062dab7b6a35b6de89af2b3f70a19b401c4
Harminder-Pal <85278472+Harminder-Pal@users.noreply.github.com>
2021-07-06 11:26:58 +0530

    Update promises.js

--- Assignment2/Promises/promises.js
+++ Assignment2/Promises/promises.js
@@ -1,1 +1,17 @@
-
+var promise = new Promise(function(resolve, reject) {
+  const x = "This is my first promise example";
+  const y = "I am ready to learn everything about it"
+  if(x === y) {
+    resolve();
+  } else {
+    reject();
+  }
+});
+  
+promise.then
+    (function () {
+        console.log('Success, Welcome tp programming');
+    },
+   function () {
+        console.log('Some error has occured');
+    });
